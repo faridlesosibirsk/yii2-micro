c29270fb6d7189a37db50899fa494d0ea319d0ac
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-15 10:49:55 +0700

    first commit

---
+++ models/top.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+
+class top  extends Model {
+    public $name = 'My Yii Application';
+}
---
+++ views/page/index.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $model app\models\top */
+
+$this->title = $model->name;//'My Yii Application';
+//$model->name;
+?>
+
+<h1><?= $this->title ?>
+</h1>
+
