39bad65fa502c7966d4509ffdeac107ff1759d49
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-18 07:30:57 +0700

    clean code

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,84 +9,21 @@ use app\models\StoreController;
 class SiteController extends Controller {
 
     private $storeController;
-    private $store;
-    private $cansel;
-    private $enter;
-    private $get;
 
     public function actionIndex() {
-        /* $storeController = new StoreController();
-          $store = $storeController->model(); */
         $this->layout = '@app/views/layouts/home';
         return $this->render('home'); /**/
     }
 
     public function actionStore() {
         $this->storeController = new StoreController();
-        //$this->model = new StoreController();
         $model = $this->storeController;
         $this->layout = '@app/views/layouts/store';
         if ($model->load(Yii::$app->request->post())) {
-            //var_dump($model['id']);
-            //$e=
             $this->storeController->store($model['id'], $model['quantity']);
-            //var_dump($e);
             return $this->render('store', ['model' => $model]);
         }
-        return $this->render('store', [
-                    'model' => $this->storeController,
-                        //'model' => $this->model,
-        ]);
-    }
-
-    public function actionCansel() {
-        $this->storeController = new StoreController();
-        $this->layout = '@app/views/layouts/store';
-        $this->cansel = $this->storeController->cansel();
-        return $this->render('store', [
-                    'model' => $this->cansel,
-        ]);
-    }
-
-    public function actionEnter() {
-        $this->storeController = new StoreController();
-        $this->enter = $this->storeController->enter();
-        $this->layout = '@app/views/layouts/store';
-        return $this->render('store', [
-                    'model' => $this->enter,
-        ]);
-    }
-
-    public function actionGet() {
-        /*$this->storeController = new StoreController();
-        $this->get = $this->storeController->get();
-        $this->layout = '@app/views/layouts/store';
-        return $this->render('store', [
-                    'model' => $this->get,
-        ]);*/
-     return $this->actionStore();
-    }
-
-    /**
-     * Login action.
-     *
-     * @return Response|string
-     */
-    public function actionLogin() {
-        $this->layout = '@app/views/layouts/store';
-        $model = new StoreController();
-        if ($model->load(Yii::$app->request->post())) {
-            //return $this->goBack();
-            var_dump($model['id']);
-            return $this->render('store', ['model' => $model,]);
-        }
-        /*
-          return $this->render('store', [
-          'model' => $model,
-          ]); */
-        //return $this->render('store', compact('model'));
-        var_dump($model['id']);
-        return $this->render('store', ['model' => $model,]);
+        return $this->render('store', ['model' => $this->storeController,]);
     }
 
 }
--- models/StoreController.php
+++ models/StoreController.php
@@ -9,9 +9,6 @@ use app\models\core\Store;
 class StoreController extends Model {
 
     private $store;
-    private $cansel;
-    private $enter;
-    private $get;
     public $id;
     public $quantity;
     public $total;
@@ -30,29 +27,7 @@ class StoreController extends Model {
     public function store($id, $quantity) {
         $this->store->getRegister()->makeNewSale();
         $this->store->getRegister()->enterItem($id, $quantity);
-        //return $this->store->getRegister()->getTotalSale();
         $this->total = $this->store->getRegister()->getTotalSale();
-    }
-
-    public function getTotal() {/*
-        if (assert($this->store)) {
-            return 0;
-        } else {
-            return $this->store->getRegister()->getTotalSale();
-        }
-        */
-    }
-
-    public function cansel() {
-        return $this->cansel;
-    }
-
-    public function enter() {
-        return $this->enter;
-    }
-
-    public function get() {
-        return $this->get;
     }
 
 }
