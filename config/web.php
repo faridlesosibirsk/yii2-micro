a8d16dd88901f75da1e073bc90ec83964821243b
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-19 07:56:12 +0700

    fix - Invalid Call – yii\base\InvalidCallException #1

--- config/web.php
+++ config/web.php
@@ -8,6 +8,13 @@ $config = [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'pdtxrk6VVh-CJI2hOnuFwlYfAWkHguGl',
         ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+            'username' => 'root',
+            'password' => '1234',
+            'charset' => 'utf8',
+        ],
     ],
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
