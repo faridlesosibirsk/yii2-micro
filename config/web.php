6e267cc996b927429a842a052134ba8602f4bdc5
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-19 09:56:26 +0700

    fix - /index.php?r=gii Forbidden (#403) #6

--- config/web.php
+++ config/web.php
@@ -24,6 +24,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
+        'allowedIPs' => ['127.0.0.1', '::1', '10.0.2.*'],
     ];
 }
 return $config;
