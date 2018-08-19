6eef07c7b19c85e645efca29b5ce7b9e082066f0
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-19 09:31:24 +0700

    fix - w3css yii2 views country index #3

--- views/country/index.php
+++ views/country/index.php
@@ -3,14 +3,35 @@
 use yii\helpers\Html;
 use yii\widgets\LinkPager;
 ?>
-<h1>Countries</h1>
-<ul>
+<div class="w3-container"> 
+    <h1>Countries</h1>
+    <table class="w3-table-all w3-hoverable">
+        <thead>
+            <tr class="w3-light-grey">
+                <th>Code</th>
+                <th>Name</th>
+                <th>Population</th>
+            </tr>
+        </thead>
         <?php foreach ($countries as $country): ?>
-        <li>
-            <?= Html::encode("{$country->code} ({$country->name})") ?>:
-        <?= $country->population ?>
-        </li>
-<?php endforeach; ?>
-</ul>
-
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+            <tr><td>
+                    <?= Html::encode("{$country->code}") ?>:
+                </td><td>
+                    <?= Html::encode("{$country->name}") ?>:
+                </td><td>
+                    <?= $country->population ?>
+                </td></tr>
+        <?php endforeach; ?>
+    </table>
+<div class="w3-padding"> 
+    <?=
+    LinkPager::widget([
+        'pagination' => $pagination,
+        'options' => [
+            'tag' => 'div',
+            'class' => 'pagination',
+        ],
+        'activePageCssClass' => 'active',
+    ])
+    ?>
+</div></div>
--- views/layouts/country.php
+++ views/layouts/country.php
@@ -13,6 +13,31 @@ use yii\helpers\Html;
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+        <style>
+li {
+    list-style-type: none;
+}
+.pagination a {
+    text-decoration: none;
+}
+.pagination {
+    display: inline-block;
+}
+
+.pagination li {
+    color: black;
+    float: left;
+    padding: 8px 16px;
+    text-decoration: none;
+}
+
+.pagination li.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+.pagination li:hover:not(.active) {background-color: #ddd;}
+        </style>
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
