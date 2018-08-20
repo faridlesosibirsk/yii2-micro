f860e985d5785dd2853434e25175ab2b16b47191
faridlesosibirsk <farid.lfsibgtu.ru@mail.ru>
2018-08-20 18:46:59 +0700

    clean code

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,28 +2,13 @@
 
 namespace app\controllers;
 
-use Yii;
 use yii\web\Controller;
-use app\models\StoreController;
 
 class SiteController extends Controller {
-
-    private $storeController;
 
     public function actionIndex() {
         $this->layout = '@app/views/layouts/home';
         return $this->render('home'); /**/
-    }
-
-    public function actionStore() {
-        $this->storeController = new StoreController();
-        $model = $this->storeController;
-        $this->layout = '@app/views/layouts/store';
-        if ($model->load(Yii::$app->request->post())) {
-            $this->storeController->store($model['id'], $model['quantity']);
-            return $this->render('store', ['model' => $model]);
-        }
-        return $this->render('store', ['model' => $this->storeController,]);
     }
 
 }
